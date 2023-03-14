cf35ea7b628fd129d30f4dbf89cf1044d2fa1026
Charles Júnior <charlesbritoneresjr@gmail.com>
2023-03-14 23:58:25 +0800

    create finishTask function

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ function submitForm(e){
 function createTask(formInput){
     let task = `
         <div class="task">
-            <input type="checkbox" name="task" id="" value="">
+            <input type="checkbox" onchange="finishTask(event)" name="task" id="" value="">
             <span>${formInput.value}</span>
             <i class="delete-task bi bi-trash" title="Delete task" onclick="deleteTask(event)"></i>
         </div>
@@ -26,4 +26,20 @@ function deleteTask(e){
     let task = e.target.parentNode;
     
     tasks.removeChild(task);
+}
+
+function finishTask(event){
+    let checkbox = event.target
+    let task = event.target.parentNode;
+    let span = task.children[1];
+    
+
+    console.log(task)
+    if(checkbox.checked){
+        // finish task
+        span.classList.toggle("finishTask");
+    }else{
+        // unfinish task
+        span.classList.toggle("finishTask");
+    }
 }
