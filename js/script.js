c7fd7591cc46157ab358c63dfe76f9b5c6a0fcd3
Charles Júnior <charlesbritoneresjr@gmail.com>
2023-03-23 21:48:14 +0800

    create editTask function and edit finishTask function

--- js/script.js
+++ js/script.js
@@ -14,8 +14,11 @@ function createTask(formInput){
     let task = `
         <div class="task">
             <input type="checkbox" onchange="finishTask(event)" name="task" id="" value="">
-            <span>${formInput.value}</span>
+            <form action="" class="form-edit-task" name="form-edit-task">
+                <input type="text" value="${formInput.value}" disabled>            
+            </form>          
             <i class="delete-task bi bi-trash" title="Delete task" onclick="deleteTask(event)"></i>
+            <i class="edit-task bi bi-pencil" title="Edit task" onclick="editTask(event)"></i>
         </div>
     `;
 
@@ -28,18 +31,42 @@ function deleteTask(e){
     tasks.removeChild(task);
 }
 
+function editTask(e){
+    // div class task
+    let task = e.target.parentNode; 
+    // form to edit task
+    let form = task.children[1];
+    // input inside form to edit task
+    let taskInput = form.firstElementChild;
+    
+    // when click on edit task icon, taskinput will be enabled and focused
+    taskInput.disabled = false;
+    taskInput.focus();
+    
+    // if the user press enter, the taskinput will be disabled and lose focus
+    form.addEventListener('submit', function(e){
+        e.preventDefault();
+        taskInput.disabled = true;
+        taskInput.blur();
+    });
+
+    // if the user click outisde the input field, the focus will be lost
+    taskInput.addEventListener('blur', function(){
+        this.disabled = true;        
+    });
+}
+
 function finishTask(event){
     let checkbox = event.target
-    let task = event.target.parentNode;
-    let span = task.children[1];
+    let form = event.target.nextElementSibling;   
+    // input inside form
+    let taskInput = form.firstElementChild;
     
-
-    console.log(task)
     if(checkbox.checked){
         // finish task
-        span.classList.toggle("finishTask");
+        taskInput.classList.toggle("finishTask");
     }else{
         // unfinish task
-        span.classList.toggle("finishTask");
+        taskInput.classList.toggle("finishTask");
     }
 }
